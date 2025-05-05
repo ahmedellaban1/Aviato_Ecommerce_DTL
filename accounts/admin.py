d21c706e900ba408a6e7b702115062deb1d4c87f
ahmedellaban1 <ellaban285@gmail.com>
2025-05-05 19:19:06 +0300

    Add CustomUser model, make migrations, and apply database changes

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import CustomUser
 # Register your models here.
+class CustomAdmin(admin.ModelAdmin):
+    list_display = ('username', 'type', 'email', 'is_superuser', 'is_staff', 'is_active')
+    search_fields = ('username', 'email')
+    list_filter = ('type', 'is_superuser', 'is_staff', 'is_active')
+
+
+admin.site.register(CustomUser, CustomAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from etc.choices import USER_TYPE_CHOICES
 # Create your models here.
+class CustomUser(AbstractUser):
+    type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.username
