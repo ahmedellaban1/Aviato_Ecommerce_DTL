44dddc22cd9a7a8d0273e4bf8181a65a6f8b1200
ahmedellaban1 <ellaban285@gmail.com>
2025-05-05 20:07:24 +0300

    Add Profile model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Profile
 # Register your models here.
 class CustomAdmin(admin.ModelAdmin):
     list_display = ('username', 'type', 'email', 'is_superuser', 'is_staff', 'is_active')
     search_fields = ('username', 'email')
     list_filter = ('type', 'is_superuser', 'is_staff', 'is_active')
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'country', 'gender')
+    search_fields = ('user', 'country', 'gender')
+    list_filter = ('gender',)
 
 admin.site.register(CustomUser, CustomAdmin)
+admin.site.register(Profile, ProfileAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from etc.choices import USER_TYPE_CHOICES
+from django_countries.fields import CountryField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from etc.choices import USER_TYPE_CHOICES, GENDER_CHOICES
 # Create your models here.
 class CustomUser(AbstractUser):
     type = models.CharField(max_length=10, choices=USER_TYPE_CHOICES)
@@ -11,5 +14,22 @@ class CustomUser(AbstractUser):
         return self.username
 
 class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    address = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='', null=True, blank=True)
+    country = CountryField(blank_label='(select country)')
+    phone = models.CharField(max_length=15)
+    date_of_birth = models.DateField(help_text="Format: YYYY-MM-DD")
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-    pass
+    @receiver(post_save, sender=CustomUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
+
+
