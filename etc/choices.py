425aa427d02b499661a2423bb15847eaefdf1aa4
ahmedellaban1 <ellaban285@gmail.com>
2025-05-11 04:04:54 +0300

    update choices

--- etc/choices.py
+++ etc/choices.py
@@ -10,3 +10,38 @@ GENDER_CHOICES = (
     ('male', 'Male'),
     ('female', 'Female'),
 )
+
+
+PRODUCT_SIZE = (
+    ('XS', 'Extra Small'),
+    ('S', 'Small'),
+    ('M', 'Medium'),
+    ('L', 'Large'),
+    ('XL', 'Extra Large'),
+    ('XXL', 'Double Extra Large'),
+    ('XXXL', 'Triple Extra Large'),
+    ('OVER', 'Oversize'),
+    ('ONE_SIZE', 'One Size Fits All'),
+    ('GEN', 'General Size'),
+)
+
+
+COLOR_CHOICES = [
+    ('#FF0000', 'Red'),
+    ('#00FF00', 'Green'),
+    ('#0000FF', 'Blue'),
+    ('#FFFF00', 'Yellow'),
+    ('#FFA500', 'Orange'),
+    ('#800080', 'Purple'),
+    ('#FFC0CB', 'Pink'),
+    ('#A52A2A', 'Brown'),
+    ('#808080', 'Gray'),
+    ('#000000', 'Black'),
+    ('#FFFFFF', 'White'),
+    ('#00FFFF', 'Cyan'),
+    ('#008080', 'Teal'),
+    ('#FFD700', 'Gold'),
+    ('#4B0082', 'Indigo'),
+    ('#F5F5DC', 'Beige'),
+    ('#D2691E', 'Chocolate'),
+]
