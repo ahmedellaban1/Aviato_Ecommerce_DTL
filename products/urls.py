ed38ae12b214ca81f2ebab5b8b3e73f11b335fcb
ahmedellaban1 <ellaban285@gmail.com>
2025-07-31 13:48:16 +0300

    update urls with all-categories

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home_page_view, shop_products_view, product_details_view
+from .views import home_page_view, shop_products_view, product_details_view, categories_view
 
 app_name = "product"
 urlpatterns = [
     path('',home_page_view, name='home-page-url'),
-    path('shop/', shop_products_view, name='shop-products-url'),
-    path('shop/product-<int:pk>', product_details_view, name='product-details-url')
+    path('shop', shop_products_view, name='shop-products-url'),
+    path('shop/product-<int:pk>', product_details_view, name='product-details-url'),
+    path('shop/categories', categories_view, name='all-categories-url'),
 
 ]
