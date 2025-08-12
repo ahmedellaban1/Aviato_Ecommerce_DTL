237cbea356965c5c737a93fa94e44c9ec1c929b0
ahmedellaban1 <ellaban285@gmail.com>
2025-08-12 22:11:25 +0300

    fix bugs in login, add sign up page, update urls with sign up view and update base_register page

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm
+from etc.choices import USER_TYPE_CHOICES
 
-# Create your views here.
+
+def create_user_view(request):
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.type = USER_TYPE_CHOICES[1][0]
+            user.set_password(form.cleaned_data['password'])
+            user.is_active = False
+            user.save()
+            return redirect() # TODO: redirect to email OTP page
+
+    else:
+        form = CreateUserForm()
+
+    context = {
+        "page_title": "Sign Up",
+        "form" : form,
+    }
+    return render(request, 'registration/sign-up.html', context)
